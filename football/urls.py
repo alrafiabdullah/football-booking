274c9ed5b1c6377380db1131913ff7f1257f1fc4
alrafiabdullah <abdullah34alrafi@gmail.com>
2022-12-16 13:09:52 +0600

    robots url added

--- football/urls.py
+++ football/urls.py
@@ -15,8 +15,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.sitemaps.views import sitemap
+from django.contrib import sitemaps
+
+from main import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
+    path('robots.txt', include('robots.urls')),
+    # path('sitemap.xml', sitemap, {
+    #      'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
+    # path('sitemap.xml', views.site_map, name='site_map'),
 ]
