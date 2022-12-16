5a1112afd1f4c6225cb7de2873ef0c06e294a413
alrafiabdullah <abdullah34alrafi@gmail.com>
2022-12-16 13:10:07 +0600

    about url added

--- main/urls.py
+++ main/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from django.contrib.sitemaps.views import sitemap
+from django.views.generic import TemplateView
 
 from . import views
 
@@ -7,4 +9,6 @@ urlpatterns = [
     path('player/register', views.register_player, name='player_register'),
     path('player/login', views.login_player, name='player_login'),
     path('player/logout', views.logout_player, name='player_logout'),
+
+    path('about', views.about, name='about'),
 ]
