191687591cb5a72eb487b03bd145ad695c499123
alrafiabdullah <abdullah34alrafi@gmail.com>
2022-12-16 22:48:39 +0600

    place added in admin

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Place
+
+admin.sites.site.index_title = 'Football Admin'
+admin.sites.site.site_header = 'Football Admin Panel'
+admin.sites.site.site_title = 'Football Admin'
+
+
+class PlaceAdmin(admin.ModelAdmin):
+    list_display = ('name', 'address', 'map_link')
+    search_fields = ('name', 'address',)
+
+
+admin.site.register(Place, PlaceAdmin)
